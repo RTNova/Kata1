b961816027fe43a5c9d5481f2b144b5a447671c4
RTNova <120527212+Spidernova@users.noreply.github.com>
2025-02-21 17:19:12 +0000

    Casi resuelto.

--- src/Kata.java
+++ src/Kata.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+// creamos total
+// recorremos array
+// creamos mayor
+// creamos menor
+// sumamos total, restamos mayor  menor
 
-public class Solution {
+public class Kata
+{
+    public static int sum(int[] numbers)
+    {
+
+        if (numbers.length <= 2 || numbers == null) {
+            return 0;
+        }
+
+        int max = numbers[0];
+        int min = numbers[0];
+        int total = 0;
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            total += numbers[i];
+
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+
+        }
+
+        return total - max - min;
+
+    }
+
 }
